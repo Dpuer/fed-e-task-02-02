c679b0194f743720796c905c25b2b63e81360777
羽扬 <392165133@qq.com>
2020-07-05 11:03:28 +0800

    eslint 学习

---
+++ code/eslint/02-eslint-perpare/02-configuration.js
@@ -0,0 +1,2 @@
+const foo = 1
+let foo2 = 2
--- code/webpack/07-dev-server/webpack.config.js
+++ code/webpack/07-dev-server/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.join(__dirname, "dist"),
     publicPath: "dist/",
   },
+  devServer: {
+    contentBase: "./public",
+  },
   module: {
     rules: [
       {
